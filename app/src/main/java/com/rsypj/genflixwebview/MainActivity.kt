3efe5b542436d631c365c4560d3a66daa76c0b89
yudha <yudha@genflix.co.id>
2020-10-12 17:17:25 +0700

    change user agent if login

--- app/src/main/java/com/rsypj/genflixwebview/MainActivity.kt
+++ app/src/main/java/com/rsypj/genflixwebview/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.rsypj.genflixwebview
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.webkit.WebResourceRequest
-import android.webkit.WebSettings
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
+import androidx.annotation.RequiresApi
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.ProtocolException
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,12 +21,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val settings = webView.settings
-        settings.userAgentString = "chrome"
         settings.javaScriptEnabled = true
         settings.domStorageEnabled = true
         webView.webViewClient = object : WebViewClient(){
             override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
                 return false
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                if (url == "https://genflix.co.id/login/email")
+                    settings.userAgentString = ""
+                else
+                    settings.userAgentString = "chrome"
             }
         }
         webView.webChromeClient = object: WebChromeClient() {
@@ -39,5 +52,27 @@ class MainActivity : AppCompatActivity() {
         }
 
         webView.loadUrl("https://genflix.co.id")
+    }
+
+    private fun handleRequest(urlString: String?): WebResourceResponse? {
+        return try {
+            val url = URL(urlString)
+            val connection: HttpURLConnection = url.openConnection() as HttpURLConnection
+            connection.setRequestProperty("User-Agent", "")
+            connection.setRequestMethod("POST")
+            connection.setDoInput(true)
+            connection.connect()
+            val inputStream: InputStream = connection.getInputStream()
+            WebResourceResponse("text/json", "utf-8", inputStream)
+        } catch (e: MalformedURLException) {
+            e.printStackTrace()
+            null
+        } catch (e: ProtocolException) {
+            e.printStackTrace()
+            null
+        } catch (e: IOException) {
+            e.printStackTrace()
+            null
+        }
     }
 }
